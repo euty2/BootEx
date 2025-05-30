1b97ddca8d8292aacd7d2b07239470062ae979d0
SJF02 <suhodo1126@gmail.com>
2025-05-30 14:33:38 +0900

    ex/ex2 메서드 구현

--- boot01/src/main/java/org/suhodo/boot01/controller/SampleController.java
+++ boot01/src/main/java/org/suhodo/boot01/controller/SampleController.java
@@ -1,7 +1,11 @@
 package org.suhodo.boot01.controller;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -29,5 +33,39 @@ public class SampleController {
         List<String> list = Arrays.asList("AAA", "BBB", "CCC", "DDD");
 
         model.addAttribute("list", list);
+    }
+
+    @GetMapping("/ex/ex2")
+    public void ex2(Model model){
+        log.info("ex/ex2.....................");
+
+        List<String> strList = IntStream.range(1, 10)
+                                .mapToObj(i->"Data"+i)
+                                .collect(Collectors.toList());
+        model.addAttribute("list", strList);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("A", "AAAA");
+        map.put("B", "BBBB");
+        model.addAttribute("map", map);
+
+        class SampleDTO{
+            private String p1, p2, p3;
+            public String getP1(){
+                return p1;
+            }
+            public String getP2(){
+                return p2;
+            }
+            public String getP3(){
+                return p3;
+            }
+        }
+         
+        SampleDTO sampleDTO = new SampleDTO();
+        sampleDTO.p1 = "Value -- p1";
+        sampleDTO.p2 = "Value -- p2";
+        sampleDTO.p3 = "Value -- p3";
+        model.addAttribute("dto", sampleDTO);
     }
 }
