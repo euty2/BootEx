4b0961105245ee985ede68e085bf251781e52983
SJF02 <suhodo1126@gmail.com>
2025-05-30 12:32:07 +0900

    DB 테스트 클래스 생성

---
+++ boot01/src/test/java/org/suhodo/boot01/DataSourceTests.java
@@ -0,0 +1,27 @@
+package org.suhodo.boot01;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.Cleanup;
+import lombok.extern.log4j.Log4j2;
+
+@SpringBootTest
+@Log4j2
+public class DataSourceTests {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    public void testConnection() throws SQLException{
+        @Cleanup
+        Connection con = dataSource.getConnection();
+    }
+}
