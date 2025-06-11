9c0ac80a7dcfb2d0224a1ccba235794c5e4617d3
SJF05 <lakors05@gmail.com>
2025-06-11 17:47:48 +0900

    썸네일일

--- boot01/src/main/java/org/suhodo/boot01/controller/BoardController.java
+++ boot01/src/main/java/org/suhodo/boot01/controller/BoardController.java
@@ -1,5 +1,12 @@
 package org.suhodo.boot01.controller;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.suhodo.boot01.dto.BoardDTO;
 import org.suhodo.boot01.dto.BoardListAllDTO;
-import org.suhodo.boot01.dto.BoardListReplyCountDTO;
 import org.suhodo.boot01.dto.PageRequestDTO;
 import org.suhodo.boot01.dto.PageResponseDTO;
 import org.suhodo.boot01.service.BoardService;
@@ -27,15 +33,17 @@ public class BoardController {
 
     private final BoardService boardService;
 
-    @GetMapping("/list")
-    public void list(PageRequestDTO pageRequestDTO, Model model){
-        
-        // PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
-        // PageResponseDTO<BoardListReplyCountDTO> responseDTO = 
-        //     boardService.listWithReplyCount(pageRequestDTO);
+    @Value("${org.suhodo.upload.path}")
+    private String uploadPath;
 
-        PageResponseDTO<BoardListAllDTO> responseDTO =
-            boardService.listWithAll(pageRequestDTO);
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model) {
+
+        // PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
+        // PageResponseDTO<BoardListReplyCountDTO> responseDTO =
+        // boardService.listWithReplyCount(pageRequestDTO);
+
+        PageResponseDTO<BoardListAllDTO> responseDTO = boardService.listWithAll(pageRequestDTO);
 
         log.info(responseDTO);
 
@@ -44,7 +52,7 @@ public class BoardController {
 
     // register.html을 보여주는 역할
     @GetMapping("/register")
-    public void registerGET(){
+    public void registerGET() {
 
     }
 
@@ -55,12 +63,12 @@ public class BoardController {
      */
     @PostMapping("/register")
     public String registerPost(@Valid BoardDTO boardDTO, BindingResult bindingResult,
-                            RedirectAttributes redirectAttributes){
-    
+            RedirectAttributes redirectAttributes) {
+
         log.info("board POST register...........");
 
         // 데이터를 넣는 과정에서 에러가 발생했다면
-        if(bindingResult.hasErrors()){
+        if (bindingResult.hasErrors()) {
             log.info("has errors...........");
             redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
 
@@ -78,8 +86,8 @@ public class BoardController {
         return "redirect:/board/list";
     }
 
-    @GetMapping({"/read", "/modify"})
-    public void read(@RequestParam(value="bno") Long bno, PageRequestDTO pageRequestDTO, Model model){
+    @GetMapping({ "/read", "/modify" })
+    public void read(@RequestParam(value = "bno") Long bno, PageRequestDTO pageRequestDTO, Model model) {
         BoardDTO boardDTO = boardService.readOne(bno);
 
         log.info(boardDTO);
@@ -89,17 +97,17 @@ public class BoardController {
 
     @PostMapping("/modify")
     public String modify(PageRequestDTO pageRequestDTO,
-                        @Valid BoardDTO boardDTO,
-                        BindingResult bindingResult,
-                        RedirectAttributes redirectAttributes){
+            @Valid BoardDTO boardDTO,
+            BindingResult bindingResult,
+            RedirectAttributes redirectAttributes) {
         log.info("board modify post............" + boardDTO);
-        
-        if(bindingResult.hasErrors()){
+
+        if (bindingResult.hasErrors()) {
             log.info("has errors............");
 
             String link = pageRequestDTO.getLink();
-            redirectAttributes.addFlashAttribute("errors", 
-                                            bindingResult.getAllErrors());
+            redirectAttributes.addFlashAttribute("errors",
+                    bindingResult.getAllErrors());
             redirectAttributes.addAttribute("bno", boardDTO.getBno());
 
             return "redirect:/board/modify?" + link;
@@ -111,16 +119,58 @@ public class BoardController {
         return "redirect:/board/read";
     }
 
+    // @PostMapping("/remove")
+    // public String remove(@RequestParam(value="bno") Long bno, RedirectAttributes
+    // redirectAttributes){
+    // log.info("remove post... " + bno);
+
+    // boardService.remove(bno);
+
+    // redirectAttributes.addFlashAttribute("result", "removed");
+
+    // return "redirect:/board/list";
+    // }
+
     @PostMapping("/remove")
-    public String remove(@RequestParam(value="bno") Long bno, RedirectAttributes redirectAttributes){
+    public String remove(BoardDTO boardDTO, RedirectAttributes redirectAttributes) {
+
+        Long bno = boardDTO.getBno();
         log.info("remove post... " + bno);
 
+        // 데이터베이스에서 삭제
         boardService.remove(bno);
+
+        log.info(boardDTO.getFileNames());
+
+        List<String> fileNames = boardDTO.getFileNames();
+
+        // 폴더에 저장된 파일들 삭제
+        if (fileNames != null && fileNames.size() > 0) {
+            removeFiles(fileNames);
+        }
 
         redirectAttributes.addFlashAttribute("result", "removed");
 
         return "redirect:/board/list";
     }
 
+    public void removeFiles(List<String> files) {
+        for (String fileName : files) {
+            Resource resource = new FileSystemResource(uploadPath + File.separator + fileName);
 
+            String resourceName = resource.getFilename();
+
+            try {
+                String contentType = Files.probeContentType(resource.getFile().toPath());
+                resource.getFile().delete();
+                if (contentType.startsWith("image")) {
+                    File thumbnailFile = new File(uploadPath + File.separator + "s_" + fileName);
+                    thumbnailFile.delete();
+                }
+
+            } catch (Exception e) {
+                log.error(e.getMessage());
+            }
+        }
+    }
 }
